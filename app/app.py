a1878cb92808571a7cc0daa361e4359dd0b4a732
GiulioNencini <142997455+GiulioNencini@users.noreply.github.com>
2026-06-17 14:59:35 +0200

    Commit semi-finale 4 -> annullate alcune modifiche

--- app/app.py
+++ app/app.py
@@ -30,8 +30,7 @@ def method_not_allowed(e):
 
 @app.route('/')
 def hello_app():
-    return render_template("index.html")
-
+    return jsonify({'msg' : 'Hello! Check about setup in documentation to use the service'}), 200
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5050)))
